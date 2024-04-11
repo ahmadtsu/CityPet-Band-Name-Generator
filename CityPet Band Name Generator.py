864e74c3b7a55a48f171f89417cb8d194bb8238b
Ahmad Al Hakim <143004240+ahmadtsu@users.noreply.github.com>
2024-04-12 00:44:56 +0300

    Add files via upload

---
+++ CityPet Band Name Generator.py
@@ -0,0 +1,10 @@
+#1. Creating a greeting for the program.
+print("Welcome to the band name generator !")
+#2. Asking the user for the city that they grew up in.
+city = input("which city did you grew up in ?\n")
+#3. Asking the user for the name of a pet.
+pet = input("what's your pet's name ?\n")
+#4. Combining the name of their city and pet and showing them their band name.
+print("your band name is: " + city + " " + pet)
+
+#made by @ahmadtsu on GitHub
